2a5a05d0da15b91d68bc818743a4a6da72d4a40a
Oley-Projects <oley.for.git@gmail.com>
2017-09-01 14:17:50 +0300

    Save Clients to database with ActiveRecord

--- app.rb
+++ app.rb
@@ -11,7 +11,31 @@ end
 class Barber < ActiveRecord::Base
 end
 
+class Contact < ActiveRecord::Base
+end
+
+before do
+	@barbers = Barber.all
+	@clients = Client.all
+end
+
 get '/' do
 	@barbers = Barber.order "created_at DESC"
 	erb :index
+end
+
+get '/visit' do
+	erb :visit
+end
+
+post '/visit' do
+	@username = params[:username]
+	@phone = params[:phone]
+	@datetime = params[:datetime]
+	@barber = params[:barber]
+	@color = params[:color]
+
+	Client.create :name => @username, :phone => @phone, :datestamp => @datetime, :barber => @barber, :color => @color
+
+	erb "<h3>Thank you, #{@username}</h3>"
 end
